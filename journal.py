d78cb647c87030bfa27412ffcb0d71b6bf8b0daf
Tystr12 <73970359+Tystr12@users.noreply.github.com>
2021-10-06 21:40:18 +0200

    first commit

---
+++ journal.py
@@ -0,0 +1,113 @@
+# Program that I can use as a journal
+# Password protected
+# Commit comment to test commits
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+my_password = os.getenv('TOKEN')
+
+my_name = 'Ty'
+
+
+def show_options() -> None:
+    '''Prints the options the user has once logged in to the diary.'''
+    print('1) -----Enter new log entry-----')
+    print('2) -----Read previous logs-----')
+    print('3) -----Exit-----')
+
+
+def print_starting_message() -> None:
+    '''Prints starting message once logged in to the program with the user's name.'''
+    print('**************************\n' +
+          f"-------{my_name}'s Journal------- \n" +
+          '**************************\n')
+
+
+def check_password(password: str) -> bool:
+    '''Checks if the password given in the parameter is the same that is saved in the .env file returns a boolean.'''
+
+    if password == my_password:
+        return True
+        print('Correct Password! Access Granted!')
+    else:
+        return False
+
+
+def change_password(password: str, new_password: str) -> None:
+    '''Changes the current password if the user can verify the current password.'''
+
+    if(check_password(password) == True):
+        my_password = new_password
+        print("password is now changed!")
+    else:
+        print("password was not changed.. try again..")
+
+
+def create_new_entry(title: str, content: str, date: str) -> None:
+    '''Creates a new diary entry and saves it to the text file.'''
+    text_file = open("something.txt", "a")
+    text_file.write
+    ('------------------------------------\n' +
+     f'{title}\n' +
+     f' date: {date}\n' +
+     f'{content}\n' +
+     '------------------------------------\n')
+    text_file.close()
+
+
+def read_the_diary() -> None:
+    '''Loops over the entries in the diary and displays prints them to the console.'''
+    text_file = open("something.txt", "r")
+    data = text_file.read().splitlines()
+    print(data)
+
+
+def save_entry_num() -> None:
+    '''Increments the amount of entries that are in the diary when a new entry is created.'''
+    text_file = open("saves.txt", "r")
+    data = text_file.read()
+    current_num = int(data[0])
+    text_file = open("saves.txt", "w")
+    res = current_num + 1
+    text_file.write(str(res))
+    text_file.close()
+
+
+def show_num_of_entrys() -> None:
+    '''Prints the amount of entries that are in the diary.'''
+    text_file = open("saves.txt", "r")
+    data = text_file.read()
+    print(f'Number of entries: {data}')
+
+
+enter_password = input('Whats the password?')
+if check_password(enter_password) == True:
+    print_starting_message()
+    running = True
+    show_options()
+    print('What would you like to do?')
+else:
+    print('Incorrect password. Try again...')
+
+while(running == True):
+    choice = input()
+    if choice == 'new entry':
+        title = input('Title?\n')
+        content = input('Write what you want to log\n')
+        date = input('What is the date?\n')
+        create_new_entry(title, content, date)
+        save_entry_num()
+        show_options()
+    if choice == 'read prev':
+        read_the_diary()
+        show_num_of_entrys()
+        show_options()
+    if choice == 'change password':
+        password = input('enter current password')
+        new_password = input('enter new password')
+        change_password(password, new_password)
+        show_options()
+    if choice == 'exit':
+        running = False
